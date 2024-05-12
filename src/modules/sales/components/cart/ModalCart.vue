aaa30242ab7235a01525942b5540968e3894c4ed
David Rivera <nicriver_40@hotmail.com>
2024-05-11 20:13:15 -0500

    iddenting code

--- src/modules/sales/components/cart/ModalCart.vue
+++ src/modules/sales/components/cart/ModalCart.vue
@@ -1,22 +1,17 @@
 <template>
-   <DialogC 
-    :closable ="false"
-   v-model:visible="visible" 
-   modal header="Cantidad" :style="{ width: '25rem' }">
-    <span class="p-text-secondary block mb-5">Ingrese Cantidad</span>
-    <div class="flex align-items-center gap-3 mb-3">
-        <label for="username" class="font-semibold w-6rem">Cantidad</label>
-       <!-- <input  type="number" v-model="quantity" /> -->
-        <InputNumberC 
-        incrementButtonClass="font-size: 10px"
-        v-model="quantity" inputId="minmax-buttons" 
-        mode="decimal" showButtons :min="0" :max="100" />
-</div>
-<div class="flex justify-content-end gap-2">
-    <ButtonC type="button"  label="Cancel" severity="secondary" @click="closeModal"></ButtonC>
-    <ButtonC :disabled="quantity === 0" type="button" label="Save" @click="onSave"></ButtonC>
-</div>
-</DialogC>
+    <DialogC :closable="false" v-model:visible="visible" modal header="Cantidad" :style="{ width: '25rem' }">
+        <span class="p-text-secondary block mb-5">Ingrese Cantidad</span>
+        <div class="flex align-items-center gap-3 mb-3">
+            <label for="username" class="font-semibold w-6rem">Cantidad</label>
+            <!-- <input  type="number" v-model="quantity" /> -->
+            <InputNumberC incrementButtonClass="font-size: 10px" v-model="quantity" inputId="minmax-buttons"
+                mode="decimal" showButtons :min="0" :max="100" />
+        </div>
+        <div class="flex justify-content-end gap-2">
+            <ButtonC type="button" label="Cancel" severity="secondary" @click="closeModal"></ButtonC>
+            <ButtonC :disabled="quantity === 0" type="button" label="Save" @click="onSave"></ButtonC>
+        </div>
+    </DialogC>
 </template>
 <script setup>
 import useModal from '../../composables/useModal';
