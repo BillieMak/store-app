3cadfdf384235c011057711e8c6369c7b9b19008
David Rivera <nicriver_40@hotmail.com>
2024-05-14 13:47:50 -0500

    format code wit prettier

--- src/modules/sales/composables/useModal.js
+++ src/modules/sales/composables/useModal.js
@@ -1,56 +1,52 @@
-import store from '@/store'
-import { computed } from 'vue'
+import { ref } from 'vue';
 
-import { ref } from "vue"
-import useCart from './useCart'
+import store from '@/store';
+import { computed } from 'vue';
 
+import useCart from './useCart';
 
 const useModal = () => {
+  const visible = computed(() => store.state.modal.visible);
 
-    const visible = computed(() => store.state.modal.visible)
+  const product = computed(() => store.getters['modal/getProduct']);
 
-    const product = computed(() => store.getters['modal/getProduct'])
+  const openModal = () => store.commit('modal/openModal');
 
-    const openModal = () => store.commit('modal/openModal')
+  const clearProduct = () => store.commit('modal/clearProduct');
 
-    const clearProduct = () => store.commit('modal/clearProduct')
+  const setProduct = (product) => store.commit('modal/setProduct', product);
 
-    const setProduct = (product) => store.commit('modal/setProduct', product)
+  const closeModal = () => {
+    clearProduct();
+    store.commit('modal/closeModal');
+  };
 
-    const closeModal = () => {
-        clearProduct()
-        store.commit('modal/closeModal')
-    }
+  const quantity = ref(1);
 
-    const quantity = ref(1)
+  const { addToCart } = useCart();
 
-    const { addToCart } = useCart()
+  const onSave = () => {
+    setTimeout(() => {
+      addToCart(product.value, quantity.value);
+      clearQuantity();
+      closeModal();
+    }, 200);
+  };
 
-    const onSave = () => {
-        setTimeout(() => {
-            addToCart(product.value, quantity.value)
-            clearQuantity()
-            closeModal()
-        }, 200)
-    }
+  const clearQuantity = () => (quantity.value = 1);
 
+  return {
+    //properties
+    visible,
+    quantity,
 
-    const clearQuantity = () => quantity.value = 1
+    //methods
+    openModal,
+    closeModal,
+    clearQuantity,
+    setProduct,
+    onSave,
+  };
+};
 
-    return {
-
-        //properties
-        visible,
-        quantity,
-
-        //methods
-        openModal,
-        closeModal,
-        clearQuantity,
-        setProduct,
-        onSave,
-    }
-
-}
-
-export default useModal
+export default useModal;
--- src/modules/sales/composables/usePayment.js
+++ src/modules/sales/composables/usePayment.js
@@ -3,50 +3,46 @@ import useCart from './useCart';
 import { useToast } from 'primevue/usetoast';
 
 const usePayment = () => {
+  const toast = useToast();
 
-    const toast = useToast();
+  const regCupon = /^FREE-[1-3]{3}$/g;
 
-    const regCupon = /^FREE-[1-3]{3}$/g
+  const { carts } = useCart();
 
-    const { carts } = useCart();
+  const cupon = ref('');
 
-    const cupon = ref('');
+  const couponApplied = ref('');
 
-    const couponApplied = ref('');
+  const discount = ref(0);
 
-    const discount = ref(0);
-
-    function calcTotalPrice(carts) {
-        let total = 0;
-        carts.forEach(({ item, quantity }) => {
-            total += item.price * quantity;
-        });
-        return parseFloat(+total.toFixed(2));
-
-    }
-
-    const totalMount = computed(() => {
-        return calcTotalPrice(carts.value) - (calcTotalPrice(carts.value) * discount.value / 100);
+  function calcTotalPrice(carts) {
+    let total = 0;
+    carts.forEach(({ item, quantity }) => {
+      total += item.price * quantity;
     });
+    return parseFloat(+total.toFixed(2));
+  }
 
+  const totalMount = computed(() => {
+    return calcTotalPrice(carts.value) - (calcTotalPrice(carts.value) * discount.value) / 100;
+  });
 
-    const validateCupon = () => {
-        if (regCupon.test(cupon.value)) {
-            discount.value = 100;
-            couponApplied.value = cupon.value;
-            cupon.value = '';
-            toast.add({ severity: 'success', summary: 'Cupon', detail: 'Cupon aplicado', life: 3000 });
-        }
+  const validateCupon = () => {
+    if (regCupon.test(cupon.value)) {
+      discount.value = 100;
+      couponApplied.value = cupon.value;
+      cupon.value = '';
+      toast.add({ severity: 'success', summary: 'Cupon', detail: 'Cupon aplicado', life: 3000 });
     }
+  };
 
+  return {
+    totalMount,
+    couponApplied,
+    cupon,
+    carts,
+    validateCupon,
+  };
+};
 
-    return {
-        totalMount,
-        couponApplied,
-        cupon,
-        carts,
-        validateCupon,
-    }
-}
-
-export default usePayment
+export default usePayment;
